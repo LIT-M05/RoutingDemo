f0dae5b15fa4781c76ef12f8b478549227eed4fc
Avrumi Friedman <alex+6@lakewoodprogramming.com>
2018-06-12 21:40:07 -0400

    Initial commit

---
+++ RoutingDemo/Controllers/GarbageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoutingDemo.Controllers
+{
+    [RoutePrefix("notgarbage")]
+    [Route("{action}")]
+    public class GarbageController : Controller
+    {
+        public void Index()
+        {
+            Response.Write("<h1>Not garbage page<h1>");
+        }
+
+        public void About()
+        {
+            Response.Write("<h1>We do garbage stuffs<h1>");
+        }
+
+    }
+}
---
+++ RoutingDemo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoutingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Url = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, "");
+            return View();
+        }
+
+        public void SomePage(int id)
+        {
+            Response.Write($"<h1>SomePage id: {id}</h1>");
+        }
+
+        [Route("baz/qux/avrumi")]
+        public void Foobar()
+        {
+            Response.Write($"<h1>Foobar page</h1>");
+        }
+
+        [Route("simchas/{simchaId}/contributions")]
+        public void Lamborghini(int simchaId)
+        {
+            Response.Write($"<h1>Contributions for the {simchaId} simcha</h1>");
+        }
+
+
+        [Route("{title}")]
+        public void Story(string title)
+        {
+            Response.Write($"<h1>Story page for {title}</h1>");
+        }
+    }
+}
